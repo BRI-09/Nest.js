25fe40e680015076aa48efbe1e3ad2ab9a4fdd27
Breneth Jian Ananayo <brenethananayo@gmail.com>
2026-02-19 23:39:36 +0800

    edited some things in the video and also deleted one

--- portfolio-v2/backend/src/gallery/gallery.service.ts
+++ portfolio-v2/backend/src/gallery/gallery.service.ts
@@ -6,12 +6,11 @@ export class GalleryService {
   { id: 1, title: 'My Cat',   alt: 'A cute cat',    src: '/assets/cat.jpg',    category: 'personal', type: 'image' },
   { id: 2, title: 'try me',  alt: 'Video blog',    src: '/assets/karaoke.mp4',   category: 'personal', type: 'video' },
   { id: 3, title: 'Flowers',  alt: 'Flowers',       src: '/assets/flowers.jpg', category: 'nature',  type: 'image' },
-  { id: 4, title: 'Sunset',   alt: 'Golden sunset', src: '/assets/sunset.mp4', category: 'nature',  type: 'image' },
+  { id: 4, title: 'Sunset',   alt: 'Golden sunset', src: '/assets/sunset.jpg', category: 'nature',  type: 'image' },
   { id: 5, title: 'Me',    alt: 'A photo of me',     src: '/assets/me3.jpg',     category: 'personal',  type: 'image' },
   { id: 6, title: 'Hobby',    alt: 'Crocheting',     src: '/assets/crochet.jpg',     category: 'personal',  type: 'image' },
   { id: 7, title: 'Hubby',    alt: 'My hubby',       src: '/assets/hoshina.jpg',     category: 'personal',  type: 'image' },
   { id: 8, title: 'Saikii',    alt: 'My hubby2',     src: '/assets/saiki.jpg',     category: 'personal',  type: 'image' },
-  { id: 9, title: 'My hubby',  alt: 'Video',    src: '/assets/hubby.mp4',   category: 'personal', type: 'video' },
   ];
 
   findAll() { return this.images; }
