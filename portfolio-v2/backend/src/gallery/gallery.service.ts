58fa3c6d99643e80ac1dc3800aa4a1e4cc855c7e
Breneth Jian Ananayo <brenethananayo@gmail.com>
2026-02-24 23:02:35 +0800

    cat

--- portfolio-v2/backend/src/gallery/gallery.service.ts
+++ portfolio-v2/backend/src/gallery/gallery.service.ts
@@ -7,7 +7,7 @@ export class GalleryService {
   { id: 2, title: 'try me',  alt: 'karaoke',    src: '/assets/karaoke.mp4',   category: 'personal', type: 'video' },
   { id: 3, title: 'Flowers',  alt: 'Flowers',       src: '/assets/flowers.jpg', category: 'nature',  type: 'image' },
   { id: 4, title: 'Cat2',   alt: 'cat', src: '/assets/cat2.jpg', category: 'nature',  type: 'image' },
-  { id: 5, title: 'cat3',    alt: 'cat',     src: '/assets/cat3.jpg',     category: 'personal',  type: 'image' },
+  { id: 5, title: 'Cat3',    alt: 'cat2',     src: '/assets/cat3.jpg',     category: 'personal',  type: 'image' },
   { id: 6, title: 'Hobby',    alt: 'Crocheting',     src: '/assets/crochet.jpg',     category: 'personal',  type: 'image' },
   { id: 7, title: 'Hubby',    alt: 'My hubby',       src: '/assets/hoshina.jpg',     category: 'personal',  type: 'image' },
   { id: 8, title: 'Saikii',    alt: 'My hubby2',     src: '/assets/saiki.jpg',     category: 'personal',  type: 'image' },
