3711d2ea2de3f893c9cf11b820b06ffe35efa801
Breneth Jian Ananayo <brenethananayo@gmail.com>
2026-02-24 22:57:56 +0800

    Edited the images and also replace some

--- portfolio-v2/backend/src/gallery/gallery.service.ts
+++ portfolio-v2/backend/src/gallery/gallery.service.ts
@@ -4,10 +4,10 @@ import { Injectable } from '@nestjs/common';
 export class GalleryService {
   private readonly images = [
   { id: 1, title: 'My Cat',   alt: 'A cute cat',    src: '/assets/cat.jpg',    category: 'personal', type: 'image' },
-  { id: 2, title: 'try me',  alt: 'Video blog',    src: '/assets/karaoke.mp4',   category: 'personal', type: 'video' },
+  { id: 2, title: 'try me',  alt: 'karaoke',    src: '/assets/karaoke.mp4',   category: 'personal', type: 'video' },
   { id: 3, title: 'Flowers',  alt: 'Flowers',       src: '/assets/flowers.jpg', category: 'nature',  type: 'image' },
-  { id: 4, title: 'Sunset',   alt: 'Golden sunset', src: '/assets/sunset.jpg', category: 'nature',  type: 'image' },
-  { id: 5, title: 'Me',    alt: 'A photo of me',     src: '/assets/me3.jpg',     category: 'personal',  type: 'image' },
+  { id: 4, title: 'Cat2',   alt: 'cat', src: '/assets/cat2.jpg', category: 'nature',  type: 'image' },
+  { id: 5, title: 'cat3',    alt: 'cat',     src: '/assets/cat3.jpg',     category: 'personal',  type: 'image' },
   { id: 6, title: 'Hobby',    alt: 'Crocheting',     src: '/assets/crochet.jpg',     category: 'personal',  type: 'image' },
   { id: 7, title: 'Hubby',    alt: 'My hubby',       src: '/assets/hoshina.jpg',     category: 'personal',  type: 'image' },
   { id: 8, title: 'Saikii',    alt: 'My hubby2',     src: '/assets/saiki.jpg',     category: 'personal',  type: 'image' },
